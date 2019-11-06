8108abd5805fdb4f3e936ed3fedfefecdbd780a6
Leandro Hurtado Salazar <42324995+LehusGH@users.noreply.github.com>
2019-11-06 15:45:38 -0500

    Prueba del SEU

---
+++ Procesador/Pruebas/SEUTB.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module SEUTB;
+	//Entradas
+	reg [1:0] seu;
+	reg [25:0] ins;
+
+	//Salidas
+	wire [63:0] value;
+
+	//Instantiate the Unit Under Test (UUT)
+	SEU uut (
+		.seu(seu), 
+		.ins(ins), 
+		.value(value)
+	);
+
+	initial begin	
+		seu = 2'b00;
+		ins = 26'b110101010101110101010;
+		#100;
+		
+		seu = 2'b01;
+		#100;
+		
+		seu = 2'b10;
+		#100;
+		
+		seu = 2'b11;
+		#100;
+	end 
+endmodule
