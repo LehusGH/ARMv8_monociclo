51583c3513e5e14d4eb8390f1b604969d509e77d
Leandro Hurtado Salazar <42324995+LehusGH@users.noreply.github.com>
2019-11-10 11:32:10 -0500

    Ahora la IM es direccionable por byte

--- Procesador/Modulos/InstructionMemory.v
+++ Procesador/Modulos/InstructionMemory.v
@@ -1,16 +1,19 @@
 `timescale 1ns / 1ps
 
-module InstructionMemory(direccion, instruccion);
-	//A la IM le entra una direccion donde buscar la instruccion en la memoria
-	input [63:0] direccion;
-	output [32:0] instruccion;
-	
-	//Simulamos la memoria como una matriz que puede contener 32 instrucciones (opcional)
-	reg [31:0] IM [31:0];
-	assign instruccion = IM[direccion];
-	
-	initial begin
-		$readmemb("IM.txt", IM);
+module InstructionMemory(
+    input [61:0] direccion,//señal enviada a traves del program counter
+    output [31:0] instruccion//instruccion a ejecutar
+    );
+
+	reg [7:0] IM [127:0];//creamos matriz para guardar las instrucciones a ejecutar
+	assign direccion = {{direccion[31:2]}, 2'b0}
+	assign instruccion[31:24] = IM[direccion];//asignamos la instruccion pedida a la salida
+	assign instruccion[31:24] = IM[direccion+1];
+	assign instruccion[23:16] = IM[direccion+2];
+	assign instruccion[15:8] = IM[direccion+3];
+	assign instruccion[7:0] = IM[direccion+4];
+
+	initial begin 
+		$readmemb("instrucciones.txt",IM);//cargamos las instrucciones 
 	end
-	
 endmodule
